af3cc5f881eb3352e471212d939674662e7190e6
Rodrigo Sánchez Molina <rsanchezm98@gmail.com>
2025-12-20 16:25:13 +0100

    defaults

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ if __name__ == "__main__":
     strava_intelligence.strava_visualizer.thunderstorm_heatmap(
         sport_types=['Run'], location="amsterdam", radius_km=20.0, add_basemap=False, show_title=False
     )
+
+    strava_intelligence.strava_visualizer.thunderstorm_heatmap(
+        sport_types=['Run'], location="madrid", radius_km=20.0, add_basemap=False, show_title=False
+    )
+
     strava_intelligence.strava_visualizer.activity_clock(
         sport_types=['Run'], show_title=False
     )
@@ -30,7 +35,11 @@ if __name__ == "__main__":
     strava_intelligence.strava_visualizer.activity_bubble_map(
         sport_types=['Run'], region="europe")
     
-    strava_year_in_sport = strava_intelligence.get_year_in_sport(year=2025, main_sport='Run', comparison_year=2020)
+    strava_year_in_sport = strava_intelligence.get_year_in_sport(year=2025, 
+                                                                 main_sport='Run', 
+                                                                 comparison_year=2024, 
+                                                                 neon_color="#de0606", 
+                                                                 comparison_neon_color="#91ffe9")
     
     # get for 2024 
     strava_year_in_sport_2024 = strava_intelligence.get_year_in_sport(year=2024, main_sport='Run', neon_color="#00ffea")
