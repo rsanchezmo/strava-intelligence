c91ba36a63bbb3aa7fbe5b08cc409e927acc4968
Rodrigo Sánchez Molina <rsanchezm98@gmail.com>
2026-01-19 22:49:39 +0100

    update main

--- main.py
+++ main.py
@@ -12,24 +12,27 @@ if __name__ == "__main__":
         sport_types=['Run'], location="amsterdam", radius_km=20.0, add_basemap=False, show_title=False
     )
 
-    strava_intelligence.strava_visualizer.thunderstorm_heatmap(
-        sport_types=['Run'], location="madrid", radius_km=20.0, add_basemap=False, show_title=False
-    )
+    # strava_intelligence.strava_visualizer.thunderstorm_heatmap(
+    #     sport_types=['Run'], location="madrid", radius_km=20.0, add_basemap=False, show_title=False
+    # )
 
-    strava_intelligence.strava_visualizer.activity_clock(
-        sport_types=['Run'], show_title=False
-    )
+    # strava_intelligence.strava_visualizer.activity_clock(
+    #     sport_types=['Run'], show_title=False
+    # )
 
-    strava_intelligence.strava_visualizer.hud_dashboard(
-        sport_type='Swim'
-    )   
+    # strava_intelligence.strava_visualizer.hud_dashboard(
+    #     sport_type='Run'
+    # )   
 
-    strava_intelligence.strava_visualizer.activity_bubble_map(
-        sport_types=['Run'], region="europe")
+    # strava_intelligence.strava_visualizer.activity_bubble_map(
+    #     sport_types=['Run'], region="europe")
+
+    weekly_data = strava_intelligence.get_weekly_report()
+    weekly_data = strava_intelligence.get_weekly_report(week_start_date="2026-01-07")
     
-    strava_year_in_sport = strava_intelligence.get_year_in_sport(year=2025, 
+    strava_year_in_sport = strava_intelligence.get_year_in_sport(year=2026, 
                                                                  main_sport='Run', 
-                                                                 comparison_year=2024, 
+                                                                 comparison_year=2025, 
                                                                  neon_color="#de0606", 
                                                                  comparison_neon_color="#91ffe9")
     
